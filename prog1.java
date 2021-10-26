8c018ae8bb72290ac257baba880f795609c089c6
Neeharika02 <78367432+Neeharika1302@users.noreply.github.com>
2021-10-26 21:12:11 +0530

    Update prog1.java

--- prog1.java
+++ prog1.java
@@ -1,8 +1,10 @@
 
 public class prog1 {
   public static void main(String[] args) {
-	String s="raja ram mohan roy";
-	s=s.replace('r','a');
-	System.out.println(s);
+	int a,b,c;
+	a=1;
+	b=2;
+	c=a+b;
+	System.out.println("output is"+c);
 }
 }
